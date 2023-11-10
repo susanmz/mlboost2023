01d59822f19373ce8910922d2240e8d7d7578d68
Susan Zhao <susanoahz@gmail.com>
2023-11-10 16:03:59 -0500

    Move data in separate folder, add Energy

--- boston.py
+++ boston.py
@@ -6,24 +6,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 import pandas as pd
 
+## Boston
+# boston = load_boston()
+# X_train, X_test, Y_train, Y_test = train_test_split(boston.data, boston.target, test_size=0.2)
 
 ## Concrete
-# df = pd.read_excel('edited_Concrete_data.xls')
+# df = pd.read_excel('data//concrete.xls')
 # df.info()
 # X = df.drop(['csMPa'], axis=1)
 # Y = df['csMPa']
 # X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-## Power
-df = pd.read_excel('Power_data.xlsx')
+## Energy - Paper looks to have used Heating Load only
+df = pd.read_excel('data//energy.xlsx')
 df.info()
-X = df.drop(['PE'], axis=1)
-Y = df['PE']
+X = df.drop(['Y1', 'Y2'], axis=1)
+Y = df['Y1']  # Heating Load - Y1 (NLL is ~0.55, like paper); Cooling Load - Y2
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-## Boston
-# boston = load_boston()
-# X_train, X_test, Y_train, Y_test = train_test_split(boston.data, boston.target, test_size=0.2)
+## Power
+# df = pd.read_excel('data//power.xlsx')
+# df.info()
+# X = df.drop(['PE'], axis=1)
+# Y = df['PE']
+# X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
 ## Wine
 # wine = fetch_ucirepo(id=186)
