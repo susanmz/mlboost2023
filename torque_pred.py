c923c4d2b29c29f04741c3faaa9b2afa4ba4557f
Susan Zhao <susanoahz@gmail.com>
2023-12-08 20:19:34 -0500

    55 strides torque estimation

--- torque_pred.py
+++ torque_pred.py
@@ -10,45 +10,46 @@ import scipy.io
 data_all_filepath = ".//data//data_all_K.mat"
 torque_all_filepath = ".//data//torq_all_K.mat"
 
+input_filepath = ".//data//input_ml.mat"
+output_filepath = ".//data//output_ml.mat"
+
 # Load the MATLAB file
 data_all = scipy.io.loadmat(data_all_filepath)
 torque_all = scipy.io.loadmat(torque_all_filepath)
 
+input_strides = scipy.io.loadmat(input_filepath)
+output_torque = scipy.io.loadmat(output_filepath)
+
 # Access the variables stored in the MATLAB file
 # Just level walking
-input_data = data_all['q_all'][:,:,:,0]  
-torque_data = torque_all['torq_all'][:,:,0] 
+# input_data = data_all['q_all'][:,:,:,0]  
+# torque_data = torque_all['torq_all'][:,:,0] 
+input_data = input_strides['result']
+torque_data = output_torque['yout']
 
-# Global Foot Angle
-# plt.plot(subj1[:,5])
-# plt.show()
+stride_test = input_data[:101,:]
+torque_test = torque_data[:101,:]
+X = input_data[101:,:]
+Y = torque_data[101:,:]
 ################# Subject 1 ############################################
-# print(val1[:,:,:,1].shape)
-subj1_X = input_data[:,:,0]  
-subj1_Y = torque_data[:,0]
+# subj1_X = input_data[:,:,0]  
+# subj1_Y = torque_data[:,0]
 
-# X = np.hstack([subj1_X[:,1], subj1_X[:,5:7], subj1_X[:,8:12]])  # data
-subj1X_test = np.concatenate((np.reshape(subj1_X[:,1], (subj1_X.shape[0],1)), subj1_X[:,5:7], subj1_X[:,8:12], np.reshape(subj1_X[:,20], (subj1_X.shape[0],1))), axis=1)
-subj1Y_test = subj1_Y
+# subj1X_test = np.concatenate((np.reshape(subj1_X[:,1], (subj1_X.shape[0],1)), subj1_X[:,5:7], subj1_X[:,8:12], np.reshape(subj1_X[:,20], (subj1_X.shape[0],1))), axis=1)
+# subj1Y_test = subj1_Y
 
-# subj2_X = input_data[:,:,1]  # Just level walking
-# subj2_Y = torque_data[:,1] # Level walking, subj 1
-# X_test2 = subj2_X
-# Y_test2 = subj2_Y
+# train_input_data = input_data[:,:,1]
+# ytrain_input_data = torque_data[:,1:]
+# for i in range(2, len(input_data[0,0])):
+#     # Vertically stack the arrays
+#     train_input_data = np.concatenate((train_input_data, input_data[:,:,i]), axis=0)
 
-
-train_input_data = input_data[:,:,1]
-ytrain_input_data = torque_data[:,1:]
-for i in range(2, len(input_data[0,0])):
-    # Vertically stack the arrays
-    train_input_data = np.concatenate((train_input_data, input_data[:,:,i]), axis=0)
-
-X = np.concatenate((np.reshape(train_input_data[:,1], (train_input_data.shape[0],1)), train_input_data[:,5:7], train_input_data[:,8:12], np.reshape(train_input_data[:,20], (train_input_data.shape[0],1))), axis=1)
-Y = np.reshape(ytrain_input_data, (ytrain_input_data.shape[0] * ytrain_input_data.shape[1],1))
+# X = np.concatenate((np.reshape(train_input_data[:,1], (train_input_data.shape[0],1)), train_input_data[:,5:7], train_input_data[:,8:12], np.reshape(train_input_data[:,20], (train_input_data.shape[0],1))), axis=1)
+# Y = np.reshape(ytrain_input_data, (ytrain_input_data.shape[0] * ytrain_input_data.shape[1],1))
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
 
-# ngb, _ = NGBRegressor(n_estimators=1000).fit(X_train, Y_train, X_test, Y_test)
-ngb, _ = NGBRegressor(n_estimators=1000).fit(X, Y)
+ngb, _ = NGBRegressor(n_estimators=1000).fit(X_train, Y_train, X_test, Y_test)
+# ngb, _ = NGBRegressor(n_estimators=1000).fit(X, Y)
 # Y_preds = ngb.predict(X_test)
 # Y_dists = ngb.pred_dist(X_test)
 
@@ -64,7 +65,7 @@ ngb, _ = NGBRegressor(n_estimators=1000).fit(X, Y)
 # plt.show()
 
 # subj1_Y_preds = ngb.predict(subj1X_test, max_iter=ngb.best_val_loss_itr)
-subj1_Y_preds = ngb.predict(subj1X_test)
+subj1_Y_preds = ngb.predict(stride_test)
 plt.plot(subj1_Y_preds)
-plt.plot(subj1Y_test)
+plt.plot(torque_test)
 plt.show()
